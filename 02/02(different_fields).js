453dd0d7039085d188f0870ecb062d5c90e767ae
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-11 12:20:42 +0200

    add 02

---
+++ 02/02(different_fields).js
@@ -0,0 +1,8 @@
+var Person = function ( name, surname ) {
+	this.name = name,
+	this.surname = surname
+}
+var a = new Person ( 'name', 'surname' ), b = new Person ( 'name', 'surname' ), c = new Person ( 'name', 'surname' )
+a.fatherName = 'fatherName'
+b.age = 'age'
+c.sex = 'sex'
