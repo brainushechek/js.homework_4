669ca90664894623dfa2bd7975a39ab6ed3747f8
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-11 12:25:45 +0200

    add 08

---
+++ 08/08(fullName).js
@@ -0,0 +1,12 @@
+var Person = function ( name, surname ) {
+	this.name = name,
+	this.surname = surname
+}
+var persons = []
+persons.push( a = new Person ( 'name1', 'surname1' ), b = new Person ( 'name2', 'surname2' ), c = new Person ( 'name3', 'surname3' ) )
+persons.push ( {name: 'name4', surname: 'surname4'} )
+for ( var i in persons ) {
+    if (persons[i].name && persons[i].surname) {
+        persons[i].fullName = 'yourFullName'
+    }
+}
