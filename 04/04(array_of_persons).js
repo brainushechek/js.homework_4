ce6a30963fe2bc834bc214b386acb233f9acbf33
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-11 12:22:28 +0200

    add 04

---
+++ 04/04(array_of_persons).js
@@ -0,0 +1,8 @@
+var Person = function ( name, surname) {
+	this.name = name,
+	this.surname = surname
+}
+
+var persons = []
+persons.push( a = new Person ( 'Name1', 'Surname1' ), b = new Person ( 'Name2', 'Surname2' ), c = new Person( 'Name3', 'Surname3') )
+persons.push( { name: 'Name4', surname: 'Surname4' } )
