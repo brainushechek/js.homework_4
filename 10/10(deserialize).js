bf5d2aed54be503d41bfaa03f0d5fdc5db39b7e0
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-11 12:27:14 +0200

    add 10

---
+++ 10/10(deserialize).js
@@ -0,0 +1,9 @@
+var Person = function ( name, surname ) {
+	this.name = name,
+	this.surname = surname
+}
+var persons = []
+persons.push( a = new Person ( 'name1', 'surname1' ), b = new Person ( 'name2', 'surname2' ), c = new Person ( 'name3', 'surname3' ) )
+persons.push(JSON.parse('{"name": "name4", "surname": "surname4"}'))
+
+
