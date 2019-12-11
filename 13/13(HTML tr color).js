d3ec8254f2963b81006a0f359ac77e3bf862aa12
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-11 12:31:19 +0200

    add 13

---
+++ 13/13(HTML tr color).js
@@ -0,0 +1,38 @@
+var Person = function ( name, surname ) {
+	this.name = name,
+	this.surname = surname
+}
+var persons = [] 
+var person = "<table border='1'>"
+persons.push( a = new Person ( 'Name1', 'Surname1' ), b = new Person ( 'Name2', 'Surname2' ), c = new Person( 'Name3', 'Surname3') )
+persons.push( { name: 'Name4', surname: 'Surname4' } )
+a.fatherName = undefined
+b.age = 'age'
+c.sex = 'sex'
+for ( var i in persons){
+    for ( var j in persons[i]) {
+	if ( i % 2 == 0 ) {
+        if (j == 'name') {
+            person += `<tr style = "background: #FFB6C1"><td>${persons[i][j]}</td>`
+        }
+        if (j == 'surname') {
+            person += `<td>${persons[i][j]}</td>`
+        }
+        if (j !== 'name' && j !== 'surname') {
+            person += `<td>${persons[i][j]}</td></tr>`
+        }
+    }   else { 
+            if (j == 'name') {
+            person += `<tr><td>${persons[i][j]}</td>`
+            }
+            if (j == 'surname') {
+            person += `<td>${persons[i][j]}</td>`
+            }
+            if (j !== 'name' && j !== 'surname') {
+            person += `<td>${persons[i][j]}</td></tr>`
+            } 
+        }
+    }
+}
+person += "</table>"
+document.write(person) 
